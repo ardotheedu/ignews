03a087e39e559e03a518a2f173772e4c6d56825a
ardotheedu <eduardosalima3@gmail.com>
2021-08-29 13:39:33 -0300

    test: Header

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,13 @@
 module.exports = {
     testPathIgnorePatterns: ["/node_modules/", "/.next/"],
     setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
+    // Usamos essa opção para que o jest entenda os arquivos .tsx .jsx .ts .js
     transform: {
         "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
     },
     testEnvironment: "jsdom",
+    // Essa opção é para que o jest entendar arquivos de estilização css sass
+    moduleNameMapper: {
+        "\\.(scss|css|sass)$": "identity-obj-proxy"
+    }
 };
